1a22951e8121968d51b986443c146852aee532bb
Abdallah <abdallah.ashraf854@gmail.com>
2026-07-03 14:41:55 +0300

    Added more funcs to load annotations from different paths and saving version in pickle file

--- volleyball_annot_loader_utils.py
+++ volleyball_annot_loader_utils.py
@@ -1,8 +1,10 @@
-import torch, cv2, os
+import torch, cv2
+import os, pickle
 from frame_box_info import BoxInfo, FrameInfo
 
 
 dataset_root = '/Users/Abdallah Salem/Desktop/group-activity-recognition'
+custom_key = lambda x: (not x.isdigit(), int(x) if x.isdigit() else x)
 
 # Data relations:
 #
@@ -10,7 +12,7 @@ dataset_root = '/Users/Abdallah Salem/Desktop/group-activity-recognition'
 #   - Each frame has annotations for 12 players
 #
 
-def load_parse_annot_lines(annot_path, ball_path):
+def load_tracking_annotation(annot_path, ball_path):
     '''
     loading annotation file and parsing it line by line according to data annotation description
     :param annot_path: data annotation file path
@@ -38,7 +40,7 @@ def load_parse_annot_lines(annot_path, ball_path):
                     frames_boxes[box.frame_id] = []
                 frames_boxes[box.frame_id].append(box)
 
-        # dictionary of frames
+        # dct of frames
         for frame_id, boxes in frames_boxes.items():
             frames_boxes[frame_id] = FrameInfo(frame_id, boxes)
 
@@ -64,7 +66,7 @@ def visualize_clips(player_annot, ball_annot, video_frames):
     '''
 
     # get frames for each video
-    frames_boxes = load_parse_annot_lines(player_annot, ball_annot)
+    frames_boxes = load_tracking_annotation(player_annot, ball_annot)
 
     for frame_id, frame in frames_boxes.items():
         img_path = os.path.join(video_frames, f'{frame_id}.jpg')
@@ -74,7 +76,7 @@ def visualize_clips(player_annot, ball_annot, video_frames):
         for box in frame:
             x1,y1,x2,y2 = box.bounding_box
             cv2.rectangle(img, (x1, y1),(x2, y2), (0, 255, 0), 2)
-            cv2.putText(img, box.annotation, (x1, y1-10),cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(img, box.category, (x1, y1-10),cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, (0, 255, 0), 2)
         cv2.circle(img, (frame.ball_info[0], frame.ball_info[1]), 8, (0, 0, 255), -1)
 
@@ -83,10 +85,115 @@ def visualize_clips(player_annot, ball_annot, video_frames):
     cv2.destroyAllWindows()
 
 
+def load_clip_annotation(annot_path):
+    '''
+    reading annotation.txt file for each clip and get clip number and it's annotation
+    :param annot_path: annotations.txt file path
+    :return: clip_annot dct
+    '''
+    clip_annot = {}
+    with open(annot_path, 'r') as file:
+        for line in file:
+            data = line.strip().split(' ')[0 : 2]
+            target_frame = data[0].replace('.jpg', '')
+            annot = data[1]
+            clip_annot[target_frame] = annot
+
+    return clip_annot
+
+
+def load_volleyball_dataset():
+    '''
+    Dataset will be loaded from two different paths then it'll be gathered in dct obj
+        - clip annotation from annotation.txt file in video dir
+        - Frame Boxes Info & Ball Info from tracking annotations dir and ball annotations dir
+    :return: video annotation dct
+    '''
+
+    # roots
+    video_root = os.path.join(dataset_root, 'videos')
+    annot_root = os.path.join(dataset_root, 'volleyball_tracking_annotation')
+    ball_root = os.path.join(dataset_root, 'volleyball_ball_annotation')
+
+    # All Annotations
+    videos_annots = {}
+
+
+    vid_dirs_names = os.listdir(video_root)
+    vid_dirs_names.sort(key=custom_key)
+
+    # loop over videos
+    for _, vid_dir_name in enumerate(vid_dirs_names):
+        vid_dir_path = os.path.join(video_root, vid_dir_name)
+
+        # Only dir. (skip files)
+        if not os.path.isdir(vid_dir_path):
+            continue
+
+        # get clips annotations
+        annot_path = os.path.join(vid_dir_path, 'annotations.txt')
+        clip_annot_dct = load_clip_annotation(annot_path)
+
+        clips_dirs_names = os.listdir(vid_dir_path)
+        clips_dirs_names.sort(key=custom_key)
+
+        clip_annot = {}
+
+        # loop over clips of each video
+        for _, clip_dir_name in enumerate(clips_dirs_names):
+            clip_dir_path = os.path.join(vid_dir_path, clip_dir_name)
+
+            # Only dir. (skip files)
+            if not os.path.isdir(clip_dir_path):
+                continue
+
+            # Aligning check
+            assert clip_dir_name in clip_annot_dct
+
+            # get frame box info & ball info
+            tracking_annot_path = os.path.join(annot_root, vid_dir_name, clip_dir_name, f'{clip_dir_name}.txt')
+            ball_path = os.path.join(ball_root, vid_dir_name, f'{clip_dir_name}.txt')
+            frames_boxes = load_tracking_annotation(tracking_annot_path, ball_path)
+
+            # group all annotation for each clip
+            clip_annot[clip_dir_name] = {
+                'annotations' : clip_annot_dct[clip_dir_name],
+                'frames_boxes_dct' : frames_boxes
+            }
+        # group clips for each video
+        videos_annots[vid_dir_name] = clip_annot
+
+    return videos_annots
+
+
+def create_pickle_version():
+    '''
+    save all annotations in pickle file version
+    '''
+    videos_annots = load_volleyball_dataset()
+    with open(f'{dataset_root}/all_annotations.pickle', 'wb') as file:
+        pickle.dump(videos_annots, file)
+
+
+def test_pickle_version():
+    '''
+    Just testing pickle file
+    '''
+
+    with open(f'{dataset_root}/all_annotations.pickle', 'rb') as file:
+        videos_annots = pickle.load(file)
+
+    frame : FrameInfo = videos_annots['0']['13456']['frames_boxes_dct']
+    print(f'Frame ID: {frame[13456].frame_id}')
+    print(f'Ball Position: {frame[13456].ball_info}')
+    print(f'Player ID: {frame[13456].boxes_info[0].player_id}')
+    print(f'Player position: {frame[13456].boxes_info[0].bounding_box}')
+    print(f'Player category: {frame[13456].boxes_info[0].category}')
+
 
 if __name__ == '__main__':
-    player_annot = f'{dataset_root}/volleyball_tracking_annotation/10/20500/20500.txt'
-    ball_annot = f'{dataset_root}/volleyball_ball_annotation/10/20500.txt'
-    video_frames = f'{dataset_root}/videos_sample/10/20500/'
+    player_annot = f'{dataset_root}/volleyball_tracking_annotation/7/51725/51725.txt'
+    ball_annot = f'{dataset_root}/volleyball_ball_annotation/7/51725.txt'
+    video_frames = f'{dataset_root}/videos_sample/7/51725/'
 
     visualize_clips(player_annot, ball_annot, video_frames)
