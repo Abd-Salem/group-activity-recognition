627135a8fc14915d64ca2c64de5fe062ab74ac96
Abdallah <abdallah.ashraf854@gmail.com>
2026-07-19 19:08:16 +0300

    adding more loaders utils for different datasets(images or features)

--- baseline_dataset_loader_utils.py
+++ baseline_dataset_loader_utils.py
@@ -2,9 +2,10 @@ import os, torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from volleyball_annot_loader_utils import load_annotations, save_annotations, config
+from PIL import Image
 
 
-class Bl1Dataset(Dataset):
+class FeatureDataset(Dataset):
     def __init__(self, features, labels):
         super().__init__()
         self.features = features
@@ -22,10 +23,53 @@ class Bl1Dataset(Dataset):
         return feature, label
 
 
+
+class ImageDataset(Dataset):
+    def __init__(self, image_paths, labels, processor=None):
+        self.paths = image_paths
+        self.labels = labels
+        self.processor = processor
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert('RGB')
+        if self.processor:
+            img = self.processor(img)
+
+        labels = torch.tensor(config.LABELS[self.labels[idx]], dtype=torch.long)
+
+        return img, labels
+
+
+
+
+def load_image_paths_labels(split):
+    '''
+    loading images paths and labels
+    :param split: train or val or test
+    :return X: paths
+    :return y: labels
+    '''
+
+    videos_annots = load_annotations()
+    X, y = [], []
+    for vid_id in split:
+        for clip_id in videos_annots[vid_id]:
+            target_frame_path = os.path.join(videos_annots[vid_id][clip_id]['clip_dir_path'], f'{clip_id}.jpg')
+            X.append(target_frame_path)
+            y.append(videos_annots[vid_id][clip_id]['label'])
+
+    return X, y
+
+
 def load_feat_annot(split):
     '''
     loading extracted features for split and corresponding labels
     :param split: train or eval or test
+    :return X: paths
+    :return y: labels
     '''
 
     annots_file = f'{config.ANNOT_ROOT}/annots.pickle'
@@ -61,7 +105,7 @@ def load_feat_annot(split):
 
 if __name__ == '__main__':
     X, y = load_feat_annot(split=config.TRAIN_IDS)
-    b1_dataset = Bl1Dataset(features=X, labels=y)
+    b1_dataset = FeatureDataset(features=X, labels=y)
     dataloader = DataLoader(b1_dataset, batch_size=2, shuffle=True)
     for idx, (x, y) in enumerate(dataloader):
         print(f'batch_idx:{idx},    x={x},      y={y}')
