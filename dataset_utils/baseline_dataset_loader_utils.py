b2fb0758114c40a268894ff8a53e8bc498f62502
Abdallah <abdallah.ashraf854@gmail.com>
2026-07-27 10:34:55 +0300

    implement loading function and dataset classes for image level and person level

--- dataset_utils/baseline_dataset_loader_utils.py
+++ dataset_utils/baseline_dataset_loader_utils.py
@@ -1,69 +1,99 @@
 import os, torch
-import numpy as np
 from torch.utils.data import Dataset
 from volleyball_annot_loader_utils import  load_volleyball_dataset
 from helper_utils.configs import CONFIG
-from helper_utils.feature_extraction import check, load_extractor_for_test
+from torchvision import transforms
 from PIL import Image
 
 # get configs
 config = CONFIG()
 
-class FeatureDataset(Dataset):
-    def __init__(self, features, labels, image_level=True):
-        super().__init__()
-        self.features = features
-        self.labels = labels
-        self.image_leve = True
-
-
-    def __len__(self):
-        return len(self.labels)
-
-
-    def __getitem__(self, idx):
-        # 9 x 2k
-        # 9 x 12 x 2k
-        if self.image_leve:
-            feature = torch.cat(self.features[idx])        # 9 x 2k
-        else:
-            feature =  torch.stack(self.features[idx])     # 9 x 12 x 2k
-        label = torch.tensor(config.LABELS[self.labels[idx]], dtype=torch.long)
-
-        return feature, label
-
-
-
-class ImageDataset(Dataset):
-    def __init__(self, image_paths, labels, processor=None):
-        self.paths = image_paths
+class PersonLevelDataset(Dataset):
+    def __init__(self, paths, labels ,processor=transforms.ToTensor(), temporal=True, clips_info=None):
+        self.paths = paths
         self.labels = labels
         self.processor = processor
+        self.temporal = temporal
+        self.clips_info = clips_info
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        img = Image.open(self.paths[idx]).convert('RGB')
-        if self.processor:
-            img = self.processor(img)
 
-        labels = torch.tensor(config.LABELS[self.labels[idx]], dtype=torch.long)
+        label = torch.tensor(config.LABELS[self.labels[idx]], dtype=torch.long)
 
-        return img, labels
+        if self.temporal:
+            frames_paths = self.paths[idx]
+            frames_info = self.clips_info[idx]      # clip's frames info
+            clip = []
+            for path_idx in range(len(frames_paths)):
+                frame = Image.open(frames_paths[path_idx]).convert('RGB')
+                frame_boxes_info = frames_info[path_idx]    # frame's boxes info
+                crops = []
+                for box_info in frame_boxes_info:
+                    processed_crop = self.processor(frame.crop(box_info.bounding_box))
+                    crops.append(processed_crop)
+                crops = torch.stack(crops)      # (players, C, H, W)
+                clip.append(crops)
+            clip = torch.stack(clip)    # (frames, players, C, H, W)
+            return clip, label
+
+        else:
+            # get target frame
+            img_path = self.paths[idx][config.TARGET_FRAME_IDX]
+            img_boxes = self.clips_info[idx][config.TARGET_FRAME_IDX]
+            img = Image.open(img_path).convert('RGB')
+
+            crops = []
+            for box_info in img_boxes:
+                processed_crop = self.processor(img.crop(box_info.bounding_box))
+                crops.append(processed_crop)
+            crops = torch.stack(crops)      # (players x C x H x W)
+            return crops, label
 
 
 
-def load_extracted_features(split, image_level=True):
-    '''
-    loading extracted features for split and corresponding labels
-    :param split: train or eval or test
-    :return X: paths
-    :return y: labels
-    '''
 
+
+class ImageLevelDataset(Dataset):
+    def __init__(self, paths, labels, processor=transforms.ToTensor(), temporal=True):
+        self.paths = paths
+        self.labels = labels
+        self.processor = processor
+        self.temporal = temporal
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+
+        label = torch.tensor(config.LABELS[self.labels[idx]], dtype=torch.long)
+
+        if self.temporal:
+            frames_paths = self.paths[idx]
+            clip = []
+            for path_idx in range(len(frames_paths)):
+                frame = Image.open(frames_paths[path_idx]).convert('RGB')
+                processed_frame = self.processor(frame)
+                clip.append(processed_frame)
+            clip = torch.stack(clip)            # (frames, C, H, W)
+
+            return clip, label
+
+        else:
+            img = Image.open(self.paths[idx][config.TARGET_FRAME_IDX]).convert('RGB')     # target frame
+            img = self.processor(img)       # (C, H, W)
+
+            return img, label
+
+
+
+
+
+def load_clips_and_labels(split:list, image_level=True):
     videos_annots = load_volleyball_dataset()
-    X, y = [], []
+    clips, labels, clips_info = [], [], []
     for vid_id in split:
         video_path = os.path.join(config.VIDEO_ROOT_DIR, vid_id)
 
@@ -72,54 +102,36 @@ def load_extracted_features(split, image_level=True):
 
         clips_ids = os.listdir(video_path)
         clips_ids.sort()
+
         for clip_id in clips_ids:
 
             if not os.path.isdir(os.path.join(video_path, clip_id)):
                 continue
 
+            label = videos_annots[vid_id][clip_id]['label']
+            labels.append(label)
+
+            frames_ids = list(videos_annots[vid_id][clip_id]['frames_boxes_dct'].keys())
+            frames_ids.sort()          # frames in each clip are in order
+
             if image_level:
-                features_dir = os.path.join(config.IMAGE_LEVEL_DIR, 'resnet')
-                frames_ids = videos_annots[vid_id][clip_id]['frames_boxes_dct']
-
-                sequence = []
-                for frame_id in frames_ids:
-                    feature_path = os.path.join(features_dir, vid_id, f'{frame_id}.npy')
-                    feature = np.load(feature_path)
-                    sequence.append(torch.from_numpy(feature))
-
+                clip = [os.path.join(video_path, clip_id, f'{frame_id}.jpg') for frame_id in frames_ids]
+                clips.append(clip)
             else:
-                features_dir = os.path.join(config.PLAYER_LEVEL_DIR, 'resnet')
-                frames_ids = videos_annots[vid_id][clip_id]['frames_boxes_dct']
-
-                sequence = []
+                clip, clip_info = [], []
                 for frame_id in frames_ids:
-                    feature_path = os.path.join(features_dir, vid_id, f'{frame_id}.npy')
-                    feature = np.load(feature_path)
-                    sequence.append(torch.from_numpy(feature).unsqueeze(0))
+                    path = os.path.join(video_path, clip_id, f'{frame_id}.jpg')
+                    clip.append(path)
 
-            X.append(sequence)
-            y.append(videos_annots[vid_id][clip_id]['label'])
+                    # get frame info object and add it to clip info list
+                    frame_info = videos_annots[vid_id][clip_id]['frames_boxes_dct'][frame_id]
+                    frame_boxes_info = frame_info.boxes_info    # list of each frame's boxes info
+                    clip_info.append(frame_boxes_info)
 
-    return X, y
+                clips.append(clip)
+                clips_info.append(clip_info)
 
-
-if __name__ == '__main__':
-    check()         # versions and machines
-
-    full_image = True      # full frame or crops
-
-    model = load_extractor_for_test()        # extractor
-
-    if full_image:
-        output_root = f'{config.IMAGE_LEVEL_DIR}/resnet'
+    if image_level:
+        return clips, labels
     else:
-        output_root = f'{config.PLAYER_LEVEL_DIR}/resnet'
-
-    # extract_features(config.VIDEO_ROOT_DIR, config.TRACKING_ANNOTS_ROOT_DIR, output_root
-    #                  , model, full_image=full_image)   # extract features and save them
-
-    x, y = load_extracted_features(['7', '10'], full_image)
-    dataset = FeatureDataset(features=x, labels=y, image_level=full_image)
-    feature, label = dataset.__getitem__(0)
-    print(f'feature shape: {feature.shape}')
-    print(f'label shape: {label.shape}')
+        return clips, labels, clips_info
