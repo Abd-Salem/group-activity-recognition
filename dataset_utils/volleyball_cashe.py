b786ca2685b425fecf45e7588e6ae5a9551a92e2
Abdallah <abdallah.ashraf854@gmail.com>
2026-08-02 15:28:13 +0300

    serialization

---
+++ dataset_utils/volleyball_cashe.py
@@ -0,0 +1,36 @@
+from volleyball_loaders import load_volleyball_dataset
+from helper_utils.configs import CONFIG
+import os, pickle
+
+def save_annotations(ball_info=False, config=None):
+    '''
+    save all annotations in pickle file version
+    :param ball_info: get or ignore ball information
+    '''
+    if config is None:
+        config = CONFIG()
+
+    videos_annots = load_volleyball_dataset(ball_info=ball_info)
+
+    if not os.path.isdir(config.ANNOT_SAVE_DIR):
+        os.makedirs(config.ANNOT_SAVE_DIR)
+
+
+    with open(f'{config.ANNOT_SAVE_DIR}/annots.pickle', 'wb') as file:
+        pickle.dump(videos_annots, file)
+
+
+
+def load_annotations(config=None):
+    '''
+    load saved annotations
+    '''
+
+    if config is None:
+        config = CONFIG()
+
+    save_file = f'{config.ANNOT_SAVE_DIR}/annots.pickle'
+    with open(save_file, 'rb') as file:
+        videos_annots = pickle.load(file)
+
+    return videos_annots
