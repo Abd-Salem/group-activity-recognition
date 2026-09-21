379f11530990991f769e262a7a8bf50588d5c1d3
Abd-Salem <abdallah.ahsraf854@gmail.com>
2026-09-22 01:22:20 +0300

    update test case

--- test/train_test.py
+++ test/train_test.py
@@ -4,28 +4,41 @@ from helper_utils.feature_extraction import get_processor
 from dataset_utils.volleyball_builders import load_clips_and_labels
 from torch.utils.data import DataLoader
 import torch.nn as nn
-import torch, math
-from pathlib import Path
+import math, pytest
 from baselines.backbones import NonTemporalBackbone
 from configs import CONFIG
 
 
-def test_trainer():
-    config = CONFIG()
 
-    # trian split
-    processor = get_processor(full_image=True, split='train')
-    train_clips, train_labels, _ = load_clips_and_labels(split=config.TRAIN_IDS, image_level=True, config=config)
-    train_dataset = ImageLevelDataset(paths=train_clips, labels=train_labels, processor=processor, temporal=False)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=config.BATCH_SIZE[1], shuffle=True)
 
-    # val split
-    processor = get_processor(full_image=True, split='val')
-    val_clips, val_labels, _ = load_clips_and_labels(split=config.VAL_IDS, image_level=True, config=config)
-    val_dataset = ImageLevelDataset(paths=val_clips, labels=val_labels, processor=processor, temporal=False)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=config.BATCH_SIZE[1], shuffle=True)
 
-    model = NonTemporalBackbone(image_level=True)
+@pytest.fixture(scope="module")
+def config():
+    return CONFIG()
+
+
+def _build_loader(config, split_name, ids, shuffle):
+    """Build an image-level DataLoader for the given split."""
+    processor = get_processor(full_image=True, split=split_name)
+    clips, labels, _ = load_clips_and_labels(split=ids, image_level=True, config=config)
+    dataset = ImageLevelDataset(paths=clips, labels=labels, processor=processor, temporal=False)
+    return DataLoader(dataset=dataset, batch_size=config.BATCH_SIZE[1], shuffle=shuffle)
+
+
+@pytest.fixture(scope="module")
+def train_loader(config):
+    return _build_loader(config, 'train', config.TRAIN_IDS, shuffle=True)
+
+
+@pytest.fixture(scope="module")
+def val_loader(config):
+    return _build_loader(config, 'val', config.VAL_IDS, shuffle=False)
+
+
+def test_trainer(config, train_loader, val_loader):
+    """Smoke test: one epoch of NonTemporalTrainer, check outputs are sane."""
+    device = config.get_device()
+    model = NonTemporalBackbone(image_level=True).to(device=device)
     optimizer = config.OPTIMS['adamw'](model.parameters(), lr=1e-4)
     criterion = nn.CrossEntropyLoss()
 
@@ -35,15 +48,13 @@ def test_trainer():
         validate_loader=val_loader,
         optimizer=optimizer,
         criterion=criterion,
-        device=config.get_device(),
-        checkpoint_dir= config.TEST_DIR,
+        device=device,
+        checkpoint_dir=config.TEST_DIR,
         model_name='test'
-        )
+    )
 
     loss, acc = trainer.train(epochs=1, test_case=True)
 
     assert math.isfinite(loss)
     assert 0.0 <= acc <= 1.0
     assert (config.TEST_DIR / 'test.pt').exists()
-
-
