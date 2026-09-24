20099d7ca65f6fbf4fd1df38621bee4597ea51c5
Abd-Salem <abdallah.ahsraf854@gmail.com>
2026-09-24 06:11:00 +0300

    fix singleton bug

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class CONFIG:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super.__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
     
 
